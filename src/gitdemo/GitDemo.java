dccfd0bc7b4a7be1beb3501e0f73cafa7f54a30d
CsoltiP <csolti@szamalk-szalezi.hu>
2024-08-26 15:04:14 +0200

    projekt

---
+++ src/gitdemo/GitDemo.java
@@ -0,0 +1,13 @@
+package gitdemo;
+
+public class GitDemo {
+
+    public static void main(String[] args) {
+        new GitDemo().kiir("alap");
+    }
+
+    private void kiir(String msg) {
+        System.out.println(msg);
+    }
+    
+}
